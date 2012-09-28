91da6e7b77ff156cf267e5b83ae89a63e54550e2
Marcus <fulbright.marcus@gmail.com>
2012-09-28 14:47:58 -0500

    Prints one month
    ruby cal.rb 02 2012 will correctly print in the command line.  The
    cal.rb file has the class and the print functionality.

--- lib/cal.rb
+++ lib/cal.rb
@@ -3,42 +3,44 @@ class Cal
   attr_reader :year
 
   def initialize( month, year )
-    @month = month
-    @year = year
+    @month = month.to_i
+    @year = year.to_i
+    @has_sixth_line = false
+    @only_has_4_lines = false
   end
 
-    def monthName
-      months = %w[January February March April May June July August September October November December]
-      myMonthName = months[@month.to_f-1]
-    end
+  def month_name
+    months = %w[January February March April May June July August September October November December]
+    myMonthName = months[@month.to_f-1]
+  end
 
-    def numDaysInMonth
-      daysInFeb = 0
-      daysInMonth = [31, daysInFeb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-      myNumDays = daysInMonth[month - 1]
+  def leap_year?
+    if @year % 400 == 0
+      true
+    elsif @year % 100 == 0
+      false
+    elsif @year % 4 == 0
+      true
+    else
+      false
     end
+  end
 
-    def leapYear?
-      if @year % 400 == 0
-        true
-      elsif @year % 100 == 0
-        false
-      elsif @year % 4 == 0
-        true
-      else
-        false
-      end
+  def days_in_feb
+    if leap_year?
+      daysInFeb = 29
+    else
+      daysInFeb = 28
     end
+  end
 
-    def daysInFeb?
-      if leapYear?
-        daysInFeb = 29
-      else
-        daysInFeb = 28
-      end
-    end
+  def number_days_in_month
+    daysInFeb = days_in_feb
+    daysInMonth = [31, daysInFeb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    daysInMonth[month - 1]      
+  end
 
-  def firstDay?
+  def first_day
     months = %w[march april may june july august september october november december january february]
     weekdays = %w[Saturday Sunday Monday Tuesday Wednesday Thursday Friday]
     day_of_month = 1
@@ -59,7 +61,119 @@ class Cal
     march_offset = ((zellerMonth +1) * 26/10).floor
     leap_year_offset = (zellerYear/4).floor + 6 *(zellerYear/100).floor + (zellerYear/400).floor
     day_of_week = (day_of_month + march_offset + zellerYear + leap_year_offset) %7
-    weekdays[day_of_week]
-end
+    
+  end
 
+  def first_day_index
+    if first_day.zero?
+      first_day_index = 6
+    else
+      first_day_index = first_day - 1
+    end
+  end
+
+  def weekday_offset
+    if first_day_index == 0
+      weekday_offset = 14
+    elsif first_day_index == 1
+      weekday_offset = 12
+    elsif first_day_index == 2
+      weekday_offset = 10
+    elsif first_day_index == 3
+      weekday_offset = 8
+    elsif first_day_index == 4
+      weekday_offset = 6
+    elsif first_day_index == 5
+      weekday_offset = 4
+    else
+      weekday_offset = 2
+    end
+    weekday_offset
+  end    
+
+  def print_headers
+    "\n     #{month_name} #{year}\nSu Mo Tu We Th Fr Sa\n"
+  end
+
+  def print_line_1
+    line_1 = ""
+    first_day_index.times do 
+      line_1 += "   "
+    end
+    (1..(7 - first_day_index)).each do |i| 
+      line_1 += " #{i} "
+    end
+      line_1.rstrip + "\n"
+  end
+
+  def print_line_2
+    line_2 = ""
+    ((8 - first_day_index)..(first_day_index + weekday_offset)).each do |i|
+      if i < 9
+        line_2 += " #{i} "
+      else
+        line_2 += " #{i}"
+      end 
+    end
+    line_2.rstrip + "\n"
+  end
+
+  def print_line_3
+    line_3 = ""
+    ((15 - first_day_index)..(first_day_index + weekday_offset + 7)).each do |i|
+      if i == 9 
+        line_3 += " #{i} "
+      else
+        line_3 += "#{i} "
+      end
+    end
+    line_3.rstrip + "\n"
+  end
+
+  def print_line_4
+    line_4 = ""
+    if number_days_in_month == (first_day_index + weekday_offset + 14)
+      @only_has_4_lines = true
+    end 
+    ((22 - first_day_index)..(first_day_index + weekday_offset + 14)).each do |i|
+    line_4 += "#{i} "
+    end
+    line_4.rstrip + "\n"
+  end
+
+  def print_line_5
+    line_5 = ""
+    if @only_has_4_lines == true
+      line_5 = "                    \n"
+    else
+      ((31 - first_day_index - 2)..(number_days_in_month)).each do |i|
+        if i > (31 - first_day_index - 2) + 6
+           @has_sixth_line = true
+          break
+        end
+        line_5 += "#{i} "
+        end
+        line_5 = line_5.rstrip + "\n"
+      end
+  line_5
+  end
+
+  def print_line_6
+    line_6 = ""
+    if @has_sixth_line == true
+       ((31 - first_day_index - 2 + 7)..(number_days_in_month)).each do |i|
+        line_6 += "#{i} "
+      end
+    else
+      line_6 = "                    "
+    end
+    line_6 += "                    \n"
+  end
+
+  def print_all
+     print_headers +  print_line_1 + print_line_2 + print_line_3 + print_line_4 + print_line_5 + print_line_6
+  end
 end
+month1 = Cal.new(ARGV[0], ARGV[1])
+puts month1.print_all
+
--- test/cal_test.rb
+++ test/cal_test.rb
@@ -15,62 +15,296 @@ end
 
 def test_03_month_name
   month1 = Cal.new(2, 1999)
-  assert_equal "February", month1.monthName
+  assert_equal "February", month1.month_name
 end
 
 def test_04_numDaysInMonth
   month1 = Cal.new(1, 1999)
-  assert_equal month1.numDaysInMonth, 31
+  assert_equal month1.number_days_in_month, 31
 end
 
 def test_04b_numDaysInMonth
   month1 = Cal.new(4, 1999)
-  assert_equal month1.numDaysInMonth, 30
+  assert_equal month1.number_days_in_month, 30
 end
 
 def test_05_leapYear_false
   month1 = Cal.new(1, 1999)
-  assert_equal month1.leapYear?, false
+  assert_equal month1.leap_year?, false
 end
 
 def test_06_leapYear_true_2004
   month1 = Cal.new(1, 2004)
-  assert_equal month1.leapYear? , true
+  assert_equal month1.leap_year?, true
 end
 
 def test_07_leapYear_true_centurian
   month1 = Cal.new(1, 2000)
-  assert_equal month1.leapYear?, true
+  assert_equal month1.leap_year?, true
 end
 
 def test_08_daysInFeb_28
   month1 = Cal.new(2, 1985)
-  assert_equal month1.daysInFeb?, 28
+  assert_equal month1.days_in_feb, 28
 end
 
 def test_09_daysInFeb_29
   month1 = Cal.new(2, 2004)
-  assert_equal month1.daysInFeb? , 29
+  assert_equal month1.days_in_feb, 29
 end
 
 def test_10_zeller
   month1 = Cal.new(9, 2012)
-  assert_equal month1.firstDay?, "Saturday"
+  assert_equal month1.first_day, 0
 end
 
 def test_11_zeller_2
   month1 = Cal.new(5, 2012)
-  assert_equal month1.firstDay?, "Tuesday"
+  assert_equal month1.first_day, 3
 end
 
 def test_12_zeller_Jan
   month1 = Cal.new(1, 2012)
-  assert_equal month1.firstDay?, "Sunday"
+  assert_equal month1.first_day, 1
 end
 
 def test_13_zeller_feb
   month1 = Cal.new(2, 2012)
-  assert_equal month1.firstDay?, "Wednesday"
+  assert_equal month1.first_day, 4
 end
 
+def test_14_print_headers
+  month1 = Cal.new(2, 2012)
+  expected = """
+     February 2012
+Su Mo Tu We Th Fr Sa
+"""
+assert_equal month1.print_headers, expected
+end
+
+def test_15_print_line_1
+  month1 = Cal.new(2, 2012)
+  expected = """          1  2  3  4
+"""
+assert_equal month1.print_line_1, expected
+end
+
+def test_16_print_line_2
+  month1 = Cal.new(2, 2012)
+  expected = """ 5  6  7  8  9 10 11
+"""
+assert_equal month1.print_line_2, expected
+end
+
+def test_17_print_line_3
+  month1 = Cal.new(2, 2012)
+  expected = """12 13 14 15 16 17 18
+"""
+assert_equal month1.print_line_3, expected
+end
+
+def test_18_print_line_4
+  month1 = Cal.new(2, 2012)
+  expected = """19 20 21 22 23 24 25
+"""
+assert_equal month1.print_line_4, expected
+end
+
+def test_20_print_line_5
+  month1 = Cal.new(2, 2012)
+  expected = """26 27 28 29
+"""
+assert_equal month1.print_line_5, expected
+end
+
+def test_21_print_all
+  month1 = Cal.new(2, 2012)
+  expected = """
+     February 2012
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29
+                    
+"""
+assert_equal month1.print_all, expected
+end
+
+def test_22_print_May_headers
+  month1 = Cal.new(5, 2012)
+  expected = """
+     May 2012
+Su Mo Tu We Th Fr Sa
+"""
+assert_equal month1.print_headers , expected
+end
+
+def test_23_print_May_line_1
+  month1 = Cal.new(5, 2012)
+  expected = """       1  2  3  4  5
+"""
+assert_equal month1.print_line_1 , expected
+end
+
+def test_24_print_May_line_2
+  month1 = Cal.new(5, 2012)
+  expected = """ 6  7  8  9 10 11 12
+"""
+assert_equal month1.print_line_2 , expected
+end
+
+def test_25_print_may_line_3
+  month1 = Cal.new(5, 2012)
+  expected ="""13 14 15 16 17 18 19
+"""
+assert_equal month1.print_line_3 , expected
+end
+
+def test_26_print_may_line_4
+  month1 = Cal.new(5, 2012)
+  expected = """20 21 22 23 24 25 26
+"""
+assert_equal month1.print_line_4 , expected
+end
+
+def test_27_print_may_line_5
+  month1 = Cal.new(5, 2012)
+  expected = """27 28 29 30 31
+"""
+assert_equal month1.print_line_5 , expected
+end
+
+def test_28_print_may_all
+  month1 = Cal.new(5, 2012)
+  expected = """
+     May 2012
+Su Mo Tu We Th Fr Sa
+       1  2  3  4  5
+ 6  7  8  9 10 11 12
+13 14 15 16 17 18 19
+20 21 22 23 24 25 26
+27 28 29 30 31
+                    
+"""
+assert_equal month1.print_all , expected
+end
+
+def test_29_print_sept_headers
+  month1= Cal.new(9, 2012)
+  expected = """
+     September 2012
+Su Mo Tu We Th Fr Sa
+"""    
+  assert_equal month1.print_headers , expected
+end
+
+def test_30_print_sept_line_1
+  month1= Cal.new(9, 2012)
+  expected = """                   1
+"""
+  assert_equal month1.print_line_1 , expected
+  end
+
+def test_31_print_sept_line_2
+  month1=Cal.new(9, 2012)
+  expected = """ 2  3  4  5  6  7  8
+"""
+ assert_equal month1.print_line_2 , expected
+end
+
+def test_32_print_sept_line_3
+  month1=Cal.new(9, 2012)
+  expected = """ 9 10 11 12 13 14 15
+"""
+  assert_equal month1.print_line_3 , expected
+  end
+
+def test_33_print_sept_line_4
+  month1=Cal.new(9, 2012)
+  expected = """16 17 18 19 20 21 22
+"""
+  assert_equal month1.print_line_4 , expected
+  end
+
+def test_34_print_sept_line_5
+  month1 = Cal.new(9, 2012)
+  expected = """23 24 25 26 27 28 29
+"""
+  assert_equal month1.print_line_5 , expected
+end
+
+def test_35_print_sept_all
+  month1 = Cal.new(9, 2012)
+  expected = """
+     September 2012
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30                     
+"""
+  assert_equal month1.print_all  , expected
+end
+
+def test_36_print_feb_2015_headers
+  month1 = Cal.new(2, 2015)
+  expected = """
+     February 2015
+Su Mo Tu We Th Fr Sa
+"""
+  assert_equal month1.print_headers , expected
+end
+
+def test_37_print_feb_2015_line_1
+  month1 = Cal.new(2, 2015)
+  expected = """ 1  2  3  4  5  6  7
+"""
+  assert_equal month1.print_line_1 , expected
+end
+
+def test_38_print_feb_2015_line_2
+  month1 = Cal.new(2, 2015)
+  expected = """ 8  9 10 11 12 13 14
+"""
+  assert_equal month1.print_line_2 , expected
+end
+
+def test_39_print_feb_2015_line_3
+  month1 = Cal.new(2, 2015)
+  expected = """15 16 17 18 19 20 21
+"""
+  assert_equal month1.print_line_3 , expected
+end
+
+def test_40_print_feb_2015_line_4
+  month1 = Cal.new(2, 2015)
+  expected = """22 23 24 25 26 27 28
+"""
+  assert_equal month1.print_line_4 , expected
+end
+
+def test_41_print_feb_2015_line_5
+  month1 = Cal.new(2, 2015)
+  expected = "                    \n"
+  assert_equal month1.print_line_5 , expected
+end
+
+def test_42_print_feb_2015_all
+  month1 = Cal.new(2, 2015)
+  expected = """
+     February 2015
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+                    
+                    
+"""
+  assert_equal month1.print_all , expected
+end 
 end
